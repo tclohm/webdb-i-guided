d1825bdfca896365dcb0454da1aa62fcfd632e92
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-01-18 16:31:17 -0800

    concepts completed

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const server = require('./server.js');
 
 const PORT = process.env.PORT || 4000;
 
+server.use((err, req, res, next) => {
+	console.log(err);
+	res.status(500).json({
+		message: "Something went wrong"
+	})
+});
+
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
--- posts/post-router.js
+++ posts/post-router.js
@@ -5,24 +5,62 @@ const db = require('../data/db-config.js');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res, next) => {
+	try {
+		// res.json( await db.select("*").from("posts") );
+		res.json( await db("posts").select() );
+	} catch (err) {
+		next(err);
+	}
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res, next) => {
+	try {
+		//const post = await db.select("*").from("posts").where("id", req.params.id);
+		const post = await db("posts").where("id", req.params.id).first(); // return object not array
+		res.json(post);
+	} catch (err) {
+		next(err);
+	}
 });
 
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res, next) => {
+	try {
+		const payload = {
+			title: req.body.title,
+			contents: req.body.contents,
+		};
+		// INSERT INTO posts (title, content) VALUES (?, ?);
+		const [id] = await db("posts").insert(payload);
+		res.json( await db("posts").where("id", id).first() )
+	} catch (err) {
+		next(err);
+	}
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id', async (req, res, next) => {
+	try {
+		const payload = {
+			title: req.body.title,
+			contents: req.body.contents,
+		};
+		// UPDATE posts SET title = ? AND contents = ? WHERE id = ?;
+		await db("posts").where("id", req.params.id).update(payload)
+		res.json(await db("posts").where("id", req.params.id).first());
+	} catch (err) {
+		next(err);
+	}
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', async (req, res, next) => {
+	try {
+		// DELETE FROM posts WHERE id = ?
+		await db("posts").where("id", req.params.id).del();
+		//res.status(204).end();
+		res.status(204).json({ message: "Deleted item" });
+	} catch (err) {
+		next(err);
+	}
 });
 
 module.exports = router;
